fd866b727ab3a251c75804a700ce5464017124a3
margofgl <methylphenidates@gmail.com>
2025-05-09 01:44:58 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import model.*;
 import service.ShoppingCart;
+import model.constants.Colour;
 
 public class Main {
     public static void main(String[] args) {
         Food meat = new Meat(5, 100);
-        Food redApple = new Apple(10, 50, "red");
-        Food greenApple = new Apple(8, 60, "green");
+        Food redApple = new Apple(10, 50, Colour.RED);
+        Food greenApple = new Apple(8, 60, Colour.GREEN);
 
         Food[] items = { meat, redApple, greenApple };
 
